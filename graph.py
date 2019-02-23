0aa11040653a2a0ae6f3f850e248d18d7b0518c0
gwatcha <michael.ampm@gmail.com>
2019-02-23 13:37:13 -0800

    remove apropos from graph and more additions

--- graph.py
+++ graph.py
@@ -1,154 +1,97 @@
-import subprocess as sp
-from shutil import which
-import json
-#regex
-import re
 # nlp
+import json
 import spacy
 
-use_subset = 1
-limit_to_commands = True
-
-def extract_raw_apropos():
-    """
-    "extracts the output of command:'apropos .' into a list of dictionaries objects  and returns it.
-    each dictionary object has form as this example:
-    {
-        name: "cd",
-        man_label: "1",
-        man_type: "Executable programs or shell commands",
-        man_apropos: "change the working directory"
-        whereis: ['/usrshare/man/mann/cd.n.gz', '/usr/share/man/man1/cd.1p.gz']
-        which: "cd: shell built-in command"
-    }
-    """
-
-    man_type_dict = {
-        '0' : 'Header files',
-        '1' : 'Executable programs or shell commands',
-        '0p' : 'Header files (POSIX)',
-        '1p' : 'Executable programs or shell commands (POSIX)',
-        '2' : 'System calls (functions provided by the kernel)',
-        '3' : 'Library calls (functions within program libraries)',
-        '3n' : 'Network Functions',
-        '3p' : 'Perl Modules (3p)',
-        '3perl' : 'Perl Modules (3perl)',
-        '4' : 'Special files (usually found in /dev)',
-        '5' : 'File formats and conventions eg /etc/passwd',
-        '6' : 'Games',
-        '7' : 'Miscellaneous  (including  macro  packages and conventions)',
-        '8' : 'System administration commands (usually only for root)',
-        '9' : 'Kernel routines',
-        'l' : 'Local documentation',
-        'n' : 'New manpages',
-    }
-
-    # regex objects for capturing components in apropos output
-    # eg output: 'zmq_z85_decode (3)   - decode a binary key from Z85 printable text'
-    name_re = re.compile('^[\w\d\-\.\:]+')
-    man_label_re = re.compile('(\(\d\))')
-    man_type_re = re.compile('(\([^\)]+\))')
-    man_apropos_re = re.compile('(?=- ).+')
-    man_re = re.compile('(?=- ).+')
-
-    whereis_re = re.compile('(\/[^ \n]+)')
-
-    apropos_list = []
-
-    # extract information from 'apropos .' to apropos_list
-    apropos_raw = None
-    if limit_to_commands:
-        apropos_raw = sp.run(['apropos', '-s', '1,8', '.'], stdout=sp.PIPE)
-    else:
-        apropos_raw = sp.run(['apropos', '.'], stdout=sp.PIPE)
-
-    # used to limit number of entries read in the case of use_subset = True
-    count = 0
-
-    for line in apropos_raw.stdout.decode().splitlines():
-        # pick apart apropos line
-        name  = name_re.search(line).group(0)
-        man_label = man_type_re.search(line).group(0).strip('()')
-        man_type = man_type_dict.get(man_label)
-        man_apropos = man_apropos_re.search(line).group(0)[2:] #trim '- '
-
-        whereis_out = []
-        if man_label != '(3)':
-            whereis_raw_out = sp.run(['whereis', name], stdout=sp.PIPE).stdout.decode()
-            whereis_out = whereis_re.findall(whereis_raw_out)
-
-        which_out = which(name)
-
-        entry = {
-            'name': name,
-            'man_label': man_label,
-            'man_type': man_type,
-            'man_apropos': man_apropos,
-            'whereis': whereis_out,
-            'which': which_out
+## to json
+# json.dumps(Graph().__dict__)
+class CmdGraph:
+    class Node:
+        "node" = {
+            "id": 0,
+            "type": "node type",
+            "label": "node label(0)",
+            "metadata": {}
         }
 
-        # finish information extract for this entry
-        apropos_list.append(entry)
+    def __init__(self, metadata, label):
+        self.graph =  {
+            "directed": True,
+            "type": "Directed Tree",
+            "label": label,
+            "metadata": metadata,
+            "nodes": [{
+                "id": 0,
+                "type": 'Base',
+                "label": 'Unix',
+                "metadata": {}
+            }],
+            "edges": []
+        }
 
-        if use_subset > 0:
-            count = count + 1
-            if count >= use_subset:
-                break
+    def add_cmd(cmd):
+        "add  entry to graph, building multiple nodes and edges in the process"
 
-    return apropos_list
+    unix_node = {
+        "id": node_id,
+        "type": "Object",
+        "label": 'None',
+        "metadata": {}
+    }
 
-def pypp_graph_infer_hypernyms():
-    "given a pypp_graph, iterates over all NOUN and VERB nodes and infers hypernymy's, the node is created and it's hyponyms become its children"
 
-def generate_graph(graph):
-    # get a nicely packaged 'apropos .' output from the system
-    apropos_list =  extract_raw_apropos()
+    doc = nlp(entry.get('man_apropos'))
 
-    #TODO model better suited to man/apropos?
-    nlp = spacy.load('en_core_web_sm')
+    # infer the object of this entry
+    for token in doc:
+        # print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+        #     token.shape_, token.is_alpha, token.is_stop)
 
-    # generate best guess nouns (objects) and verbs (commands) from entry.man_apropos
-    for entry in apropos_list:
-        doc = nlp(entry.get('man_apropos'))
+        # dobj is the object acted on by the verb, so it is our object
+        if token.dep_ == 'dobj'
 
-        # extract spacy's tagged NOUN chunks and VERBS
-        for token in doc:
-            # print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
-                # token.shape_, token.is_alpha, token.is_stop)
-            if token.pos_ == 'VERB':
-                verbs.append(token.text)
+        # this is the action
+        if token.tag_ == 'VERB' and token.dep_ == 'ROOT':
+            print(token.text)
 
-        for nc in doc.noun_chunks:
-            nouns.append(nc)
+    for nc in doc.noun_chunks:
+        nouns.append(nc)
 
         # TODO command + flag UNIX nodes obtained from parsing man page as well
         # as subcommands, such as for ip or for docker
         # if X is command:
         #     add_additional_nodes()
 
+    # TODO attach node to base
 
+    # TODO move node down tree if its OBJECT is a hyponym of any of it's OBJECT
+    # non-unix child nodes.
+
+    # TODO move node down tree if its COMMAND is a hyponym of
+    # any of it's non-unix child nodes
+
+    # TODO 'specify node', meaning if a COMMAND or OBJECT node has >5 unix
+    # nodes attached, look through their descriptions and try to infer
+    # the hyponym that is shared the most among them - then create that node
+    # and move its hypernym's unix commands underneath it.
 
 def create_graph():
-    "creates the graph. \
- returns a graph represented by an adjacency list with entries: [ apropos_entry, type, apropos_desc, path, NOUN, VERB ] \
-    where NOUN is the subject of the command and VERB is the action on that subject"
-
-    # see README.org for further specification
-    nodes = []
-    edges = []
-    graph =  {
-         "directed": True,
-         "type": "Directed Tree",
-         "label": "Unix manual visualization",
-         "metadata": None,
-         "nodes": nodes,
-         "edges": edges
-     }
-
-    generate_graph(graph)
+    "returns a graph represented by a list of nodes and list of edges"
 
 
+
+    apropos_list =  extract_raw_apropos()
+
+    ### generate nodes and edges with POS-Tagger ###
+
+    #TODO model better suited to man/apropos?
+    #TODO custom tagger rules for the unix universe (eg. load always verb, return always verb)
+    nlp = spacy.load('en_core_web_sm')
+    # use spacy POS tagger on the words in each apropos description
+    # the NOUN dobj is an Object, the VERB (commands) from entry.man_apropos
+    for entry in apropos_list:
+
+
+        add_node(graph, entry)
 
 
 
